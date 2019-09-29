d191215a48ced7ac779eee46f4f6493bd8915192
Mihail <m.iurco@elseviercom>
2019-09-29 10:05:34 +0100

    dev_workflow_update_path_on_the_go

--- dev-workflow/gapp.sh
+++ dev-workflow/gapp.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+#
 # AIM OF THIS SCRIPT IS TO SPEED UP PERSONAL WORKFLOW
 #
 # SHORCUT OF THE FOLLOWING ACTIONS
-#				 1. GIT ADD
+# 			 1. GIT ADD
 # 			 2. GIT COMMIT
-#				 3. GIT PUSH
+# 			 3. GIT PUSH
 #
 # HOW TO USE ?
 # 			$ gapp the_file commit_message 
-#				$ gapp th_directory commit_message
+# 			$ gapp th_directory commit_message
 
 case "$1" in 
 	"") echo "Usage: ${0##*/} <filename>"; exit $E_PARAM;;
